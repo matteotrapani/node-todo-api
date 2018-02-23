ef8bed6c2234420764526ed0c01be78cdfb8b4d8
Matteo Trapani <matteo.trapani@ennova-research.com>
2018-02-23 11:03:24 +0100

    First setup of REST API server

--- server/server.js
+++ server/server.js
@@ -1,60 +1,23 @@
-const mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var {mongoose} = require("./db/mongoose");
+var {Todo} = require("./models/todo");
+var {User} = require("./models/user");
 
-var Todo = mongoose.model('Todo', {
-    text: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    },
-    completedAt: {
-        type: Number,
-        default: null
-    }
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo(req.body);
+    todo.save().then(doc => {
+        res.send(doc);
+    }, e => {
+        res.status(400).send(e);
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Started on port 3000');
 });
-
-var saveOnSuccess = doc => {
-    console.log(JSON.stringify(doc, undefined, 2));
-};
-
-var saveOnError = err => {
-    console.log('Unable to save Todo', err);
-};
-
-// var newTodo = new Todo({
-//     text: 'Exercise 1',
-// });
-
-// newTodo.save().then(doc => {
-//     console.log('Saved Todo', doc);
-// }, err => {
-//     // console.log(err);
-//     console.log('Unable to save Todo');
-// });
-
-// var newTodo = new Todo({
-//     text: 'Check your code!!!'
-// });
-
-// newTodo.save().then(saveOnSuccess, saveOnError);
-
-var User = mongoose.model('User', {
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 1
-    }
-});
-
-var newUser = new User({
-});
-
-newUser.save().then(saveOnSuccess, saveOnError);
