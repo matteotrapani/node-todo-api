558e28630d3dda3d0fc7ed2eba50904325e4a0ad
Matteo Trapani <matteo.trapani@ennova-research.com>
2018-03-02 17:15:37 +0100

    Hash user psw before save

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,17 +1,32 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-}
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     });
+// });
 
-console.log(token);
+var hashedPassword = '$2a$10$47VH41fTQvYhEBueYTVZnODmgwV5SlN2N5jguEWypolwYojVXXy7G';
 
-var decoded = jwt.verify(token, '123abc');
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
 
-console.log('decoded', decoded);
+// var data = {
+//     id: 10
+// }
+
+// var token = jwt.sign(data, '123abc');
+
+// console.log(token);
+
+// var decoded = jwt.verify(token, '123abc');
+
+// console.log('decoded', decoded);
 
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
